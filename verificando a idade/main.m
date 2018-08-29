c0304fa4545cc4408ad5d0c963b3e3091048c694
jairrayas <jairrayas@outlook.com>
2018-08-29 17:41:32 -0300

    arrumando erros

--- verificando a idade/main.m
+++ verificando a idade/main.m
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<locale.h>
 
 int main(void)
 {
+    setlocale(LC_ALL,"Portuguese");
+    
     int idade;
     
     printf("Digite a sua idade: ");
     scanf("%d", &idade);
     
-    if(idade >= 0) || (idade <12){
-        printf("Crianca");
+    if(idade >= 0 && idade < 12){
+        printf("Criança");
     }
-    else if(idade >= 12) && (idade <18){
-        printf("Adolscente");
+    else if(idade >= 12 && idade < 18){
+        printf("Adolescente");
     }
-    else if(idade >= 18) && (idade <60){
+    else if(idade >= 18 && idade < 60){
         printf("Adulto");
     }
-    else(idade >= 60)
+    else if(idade >= 60){
         printf("Idoso");
     }
 
-    returm 0;
+    printf("\n\n");
+    
+    return 0;
 }
